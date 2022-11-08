cd2a2701c65d526640778b7adc10e0ab89fb6dd0
VinlaRose <109898811+VinlaRose@users.noreply.github.com>
2022-11-08 23:18:53 +0530

    added

---
+++ Q15,16,17/app.js
@@ -0,0 +1,18 @@
+
+
+let Ram = {
+    age: 25,
+    yuga: "Treta"
+
+}
+let Krishna = {
+    age: 31,
+    yuga: "Dwapar"
+
+}
+
+function moreAge(){
+    return Ram.age;
+}
+console.log(moreAge())
+
