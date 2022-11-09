ed123f62a08be9707c0ed19af7c506670fcbac01
VinlaRose <109898811+VinlaRose@users.noreply.github.com>
2022-11-09 09:19:01 +0530

    Q15,16,17 done

--- Q15,16,17/app.js
+++ Q15,16,17/app.js
@@ -1,18 +1,52 @@
 
 
-let Ram = {
+let person1 = {
+    name: "Ram",
     age: 25,
-    yuga: "Treta"
+    yuga: "Treta",
+    power: 2500
 
 }
-let Krishna = {
+let person2 = {
+    name : "Krishna",
     age: 31,
-    yuga: "Dwapar"
+    yuga: "Dwapar",
+    power: 2325
 
 }
 
-function moreAge(){
-    return Ram.age;
+function moreAge(a,b){
+   if(a.age > b.age){
+    return a.name;
+   }else{
+    return b.name;
+   }
 }
-console.log(moreAge())
 
+function morePower(a,b){
+    if(a.power > b.power){
+     return a.name;
+    }else{
+     return b.name;
+    }
+ }
+
+ function newMorePower(a,b){
+    let newPow1 = a.power + a.name.length;
+    let newPow2 = b.power + b.name.length;
+    if(newPow1>newPow2){
+        return a.name;
+    }else{
+        return b.name;
+    }
+
+ }
+
+
+console.log(moreAge(person1, person2));
+console.log(morePower(person1, person2));
+console.log(newMorePower(person1,person2));
+
+
+
+ 
